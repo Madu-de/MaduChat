1edf514a7da11a2b80856003967e72dba1a5a7e8
Madu <kontakt.madu@gmail.com>
2023-08-19 16:34:52 +0200

    Settingsoption: showAvatar work now

--- frontend/src/app/chat/message/message.component.ts
+++ frontend/src/app/chat/message/message.component.ts
@@ -1,12 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Message } from 'src/app/classes/Message';
+import { Settings } from 'src/app/classes/Settings';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'chat-message',
   templateUrl: './message.component.html',
   styleUrls: ['./message.component.scss']
 })
-export class MessageComponent {
+export class MessageComponent implements OnInit {
   @Input()
   message: Message | undefined;
+
+  constructor(public userService: UserService) {}
+
+  settings: Settings | undefined;
+
+  ngOnInit() {
+    this.userService.getMe(false, false, true).subscribe(user => {
+      this.settings = user.settings;
+    }); 
+  }
 }
