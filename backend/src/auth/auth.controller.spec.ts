4a5d7da534cb5c1f99b4eb778b9cebdf609389c7
Madu <kontakt.madu@gmail.com>
2023-08-27 15:52:04 +0200

    #11 Auth tests

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { GuardMock } from '../../test/guardMock';
 import { UserService } from '../user/user.service';
+import { User } from 'src/user/user';
 
 describe('AuthController', () => {
   let controller: AuthController;
+
+  const token = 'testtoken';
+  const user = <User>{
+    name: 'testuser',
+    username: 'testuser',
+    email: 'testuser@example.com',
+    password: 'testpassword',
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,7 +22,11 @@ describe('AuthController', () => {
         {
           provide: AuthService,
           useValue: {
-            signIn: jest.fn(),
+            signIn: jest.fn(() => {
+              return new Promise(resolve => {
+                resolve(token);
+              });
+            }),
           },
         },
         {
@@ -24,15 +36,20 @@ describe('AuthController', () => {
           },
         },
       ],
-    })
-      .overrideProvider(AuthService)
-      .useValue(GuardMock)
-      .compile();
+    }).compile();
 
     controller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should log in', async () => {
+    expect(await controller.signIn({})).toBe(token);
+  });
+
+  it('should register', async () => {
+    expect(await controller.register(user)).toBe(token);
   });
 });
