9a1787d46198ccdc5995ee9bb6b549bb73fd129d
Madu <kontakt.madu@gmail.com>
2023-12-01 18:00:47 +0100

    Feature #84 Frontend tests

--- frontend/src/app/basic-needs/choose-users-popup/choose-users-popup.component.spec.ts
+++ frontend/src/app/basic-needs/choose-users-popup/choose-users-popup.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ChooseUsersPopupComponent } from './choose-users-popup.component';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { UserService } from 'src/app/services/user.service';
+import { of } from 'rxjs';
+import { LanguageService } from 'src/app/services/language.service';
+import { MatListModule } from '@angular/material/list';
 
 describe('ChooseUsersPopupComponent', () => {
   let component: ChooseUsersPopupComponent;
@@ -8,7 +13,36 @@ describe('ChooseUsersPopupComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ChooseUsersPopupComponent]
+      imports: [
+        MatDialogModule,
+        MatListModule
+      ],
+      declarations: [ChooseUsersPopupComponent],
+      providers: [
+        {
+          provide: MatDialogRef,
+          useValue: {}
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {
+            users: [],
+            clientUser: {}
+          }
+        },
+        {
+          provide: UserService,
+          useValue: {
+            getMe: () => of({})
+          }
+        },
+        {
+          provide: LanguageService,
+          useValue: {
+            getValue: () => ''
+          }
+        },
+      ]
     });
     fixture = TestBed.createComponent(ChooseUsersPopupComponent);
     component = fixture.componentInstance;
--- frontend/src/app/friends/user-list/friend-user/friend-user.component.spec.ts
+++ frontend/src/app/friends/user-list/friend-user/friend-user.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FriendUserComponent } from './friend-user.component';
+import { UserService } from 'src/app/services/user.service';
+import { SnackbarService } from 'src/app/services/snackbar.service';
+import { LanguageService } from 'src/app/services/language.service';
+import { MockModule } from 'ng-mocks';
+import { BasicNeedsModule } from 'src/app/basic-needs/basic-needs.module';
 
 describe('FriendUserComponent', () => {
   let component: FriendUserComponent;
@@ -8,7 +13,24 @@ describe('FriendUserComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [FriendUserComponent]
+      imports: [
+        MockModule(BasicNeedsModule)
+      ],
+      declarations: [FriendUserComponent],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {}
+        },
+        {
+          provide: SnackbarService,
+          useValue: {}
+        },
+        {
+          provide: LanguageService,
+          useValue: {}
+        },
+      ]
     });
     fixture = TestBed.createComponent(FriendUserComponent);
     component = fixture.componentInstance;
