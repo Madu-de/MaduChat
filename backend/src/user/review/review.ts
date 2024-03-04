1103b10e0447f1c65cc1904116c16b4236fa45eb
Madu <kontakt.madu@gmail.com>
2024-03-04 21:06:51 +0100

    Begin database

---
+++ backend/src/user/review/review.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../user';
+import { IsString, Max, MaxLength, Min, MinLength } from 'class-validator';
+
+@Entity()
+export class Review {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User, {
+    cascade: true,
+  })
+  @JoinTable()
+  author: User;
+
+  @ManyToOne(() => User, {
+    cascade: true,
+  })
+  @JoinTable()
+  target: User;
+
+  @Column({
+    type: 'text',
+  })
+  @IsString()
+  @MinLength(1)
+  @MaxLength(500)
+  text: string;
+
+  @Column({
+    type: 'integer',
+  })
+  @Min(1)
+  @Max(5)
+  stars: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
